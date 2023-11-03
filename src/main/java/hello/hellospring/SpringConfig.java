6a726248be083b7d280a765ea4c798bb4eef8ca6
LeeSejin <dltpwls1203@gmail.com>
2023-11-03 14:37:35 +0900

    JpaTest

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
 
     private EntityManager em;
 
@@ -18,7 +24,7 @@ public class SpringConfig {
         this.em = em;
     }
 
-    /*
+
     private DataSource dataSource;
 
     @Autowired
@@ -28,14 +34,15 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
+    /*
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
         // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        // return new JpaMemberRepository(em);
     }
+     */
 }
